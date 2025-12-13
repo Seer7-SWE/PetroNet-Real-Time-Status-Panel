6b00d194c7764169102e0c952c971812cfdf6318
Seer7-SWE <seeralan006@gmail.com>
2025-12-13 11:46:09 +0300

    dashboard.jsx issue fixed

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -5,12 +5,6 @@ import AlertsPanel from "./AlertsPanel";
 import RechartsPanel from "./RechartsPanel";
 
 
-onValue(sensorsRef, (snapshot) => {
-  const data = snapshot.val();
-  console.log("🔥 FIREBASE SENSOR DATA:", data);
-  setSensors(data || {});
-});
-
 export default function Dashboard() {
   return (
     <div className="dashboard">
